50cc5fb083bebc6bb2dfa18c3c7b924c9f1ae034
rls1004 <94449326+rls1004ubu@users.noreply.github.com>
2022-02-28 22:59:19 +0100

    Add files via upload

---
+++ casoEstudio2/src/pg01/c02/Thread_.java
@@ -0,0 +1,24 @@
+package pg01.c02;
+
+public class Thread_ implements Runnable{
+
+	@Override
+	public void run() {
+		for(int i=0; i<1000000; i++) {
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			for(int j = 0; j<50000; j++) {
+				System.out.print("- ");
+			}
+		
+		}
+		
+		System.out.println();
+	}
+
+}
